6fcdf5d422ad7a662de88bfd5daa9c3deca864db
huallpa.nestor <huallpa.nestor@c43cf7e0-09d8-3d3d-55b7-b466cd00da44>
2011-06-06 04:11:48 +0000

    instale tamplates para scaffolding

--- trunk/LibraryApp/grails-app/domain/com/library/Library.groovy
+++ trunk/LibraryApp/grails-app/domain/com/library/Library.groovy
@@ -6,14 +6,30 @@ class Library {
 	String name
 	byte[] photo
 	String homepage
-	String mail
+	String email
 	String phone
+	
+	Location location
 
     static constraints = {
 		libraryId(size: 3..20, unique: true)
+		name(size: 3..60)
 		photo(nullable: true)
 		homepage(url: true, nullable: true)
-		mail(nullable: true)
+		email(email: true, nullable: true)
 		phone(nullable: true)
+		location(nullable: true)
     }
+	
+	static mapping = {
+		location lazy: false
+	}
+	
+	void toComment(String comment, String User) {
+		
+	}
+	
+	Boolean isNearOf(Location location, Integer maxDistanceKm) {
+		return false;
+	}
 }
--- trunk/LibraryApp/grails-app/domain/com/library/Location.groovy
+++ trunk/LibraryApp/grails-app/domain/com/library/Location.groovy
@@ -2,14 +2,22 @@ package com.library
 
 class Location {
 
+	static belongsto = Library
+	
+	String country
+	String city
 	String street
 	Integer number
-	String city
-	String country
 	
 	String latitude
 	String longitude
 	
     static constraints = {
+		country(maxsize: 60,nullable:true)
+		city(maxsize: 60, nullable:true)
+		street(maxsize: 60, nullable:true)
+		number(maxsize: 20, nullable:true)
+		latitude(maxsize: 10, nullable:true)
+		longitude(maxsize: 10, nullable:true)
     }
 }
