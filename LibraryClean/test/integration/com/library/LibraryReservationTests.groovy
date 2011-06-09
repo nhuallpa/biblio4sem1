09e384121f8af62ec182c112b7de66f828b4a597
huallpa.nestor <huallpa.nestor@c43cf7e0-09d8-3d3d-55b7-b466cd00da44>
2011-06-09 20:58:42 +0000

    Test de integracion bloker y css

---
+++ LibraryClean/test/integration/com/library/LibraryReservationTests.groovy
@@ -0,0 +1,17 @@
+package com.library
+
+import grails.test.*
+
+class LibraryReservationTests extends GroovyTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
