9f79c97ac50aee4980e418fdca58a9bd007cf8f3
huallpa.nestor <huallpa.nestor@c43cf7e0-09d8-3d3d-55b7-b466cd00da44>
2011-06-09 20:58:42 +0000

    Test de integracion bloker y css

---
+++ trunk/LibraryClean/test/integration/com/library/LibraryBookTests.groovy
@@ -0,0 +1,35 @@
+package com.library
+
+import grails.test.*
+
+class LibraryBookTests extends GroovyTestCase {
+	String libId
+	String nameNew
+	Library library
+	
+    protected void setUp() {
+        super.setUp()
+		nameNew = 'Ateneo'
+		libId = 'ateneoBA'
+		library = new Library()
+		library.setLibraryId libId
+		library.setName nameNew
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testAddReservation() {
+//		Book book = new Book( title:'Grails', subject:'dev', state:'A' )
+//		User user = new User( name:'nestor')
+//		Reservation reservation = new Reservation(book, user)
+//		library.addToReservations(reservation)
+//				
+//		assertNotNull library.save()
+    }
+	void testDeleteReservation() {
+		
+	}
+	
+}
