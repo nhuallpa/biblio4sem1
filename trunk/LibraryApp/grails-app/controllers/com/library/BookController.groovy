4ff5b35be846463f4be9b9541fd3fe79c9fad6a2
arielik@gmail.com <arielik@gmail.com@c43cf7e0-09d8-3d3d-55b7-b466cd00da44>
2011-06-14 20:25:01 +0000

    Se agrego soporte para search, y muestra los resultados en la misma pagina! Ademas muestra un mensaje indicando ocurrencias.

--- trunk/LibraryApp/grails-app/controllers/com/library/BookController.groovy
+++ trunk/LibraryApp/grails-app/controllers/com/library/BookController.groovy
@@ -12,6 +12,14 @@ class BookController {
 	}
 	def searchBookHome = {
 	}
+	def search = {
+		//render Book.search(params.q, params)
+		def searchResults = Book.search(params.q, params)
+		flash.message = "${searchResults.total} results found for search: ${params.q}"
+		flash.q = params.q
+		return [searchResults:searchResults.results, resultCount:searchResults.total]
+	}
+	
 	def searchBookByTitle = {
 		def books = Book.list()
 		redirect(controller:'book', action:'searchBookHome', bookList:books)
