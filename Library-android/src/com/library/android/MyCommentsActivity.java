7dc0994d27b578bb8af5ac003a180b754889508c
lalosoft <lalosoft@c43cf7e0-09d8-3d3d-55b7-b466cd00da44>
2011-07-11 15:58:13 +0000

    MyComments in progress...

---
+++ Library-android/src/com/library/android/MyCommentsActivity.java
@@ -0,0 +1,118 @@
+package com.library.android;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.library.android.domain.Comment;
+import com.library.android.mock.BooksMock;
+import com.library.android.mock.UserMock;
+
+	public class MyCommentsActivity extends ListActivity {
+		
+		private SimpleAdapter simpleAdapter = null; 
+		private ArrayList<HashMap<String, String>> list;
+		private Handler mHandler = new Handler();
+		
+
+		public void OnCreate(Bundle b){
+			super.onCreate(b);
+			setContentView(R.layout.my_comments);
+		}
+		
+	
+	
+
+	public void onStart(){
+		super.onStart();
+
+		LibraryAsyncTask task = new LibraryAsyncTask();
+		
+		task.execute();
+	}
+	
+	private class LibraryAsyncTask extends AsyncTask<Void, Context, ArrayList<Comment>>{
+
+		@Override
+		protected ArrayList<Comment> doInBackground(Void... arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(ArrayList<Comment> results){
+			
+			list = new ArrayList<HashMap<String,String>>();
+			
+			if(results.size() > 0) {
+				
+
+				for (Comment dato : results) {
+					HashMap<String,String> temp = new HashMap<String,String>();
+	
+						//TODO: quisiera que salga el picture de cada usuario!!
+//						try {
+							
+//							temp.put("picture", "http://graph.facebook.com/" + dato.getJSONObject("from").getString("id")+ "/picture");
+//							temp.put("name", dato.getJSONObject("from").getString("name"));
+//							temp.put("post", dato.getString("message"));
+//							temp.put("fecha", parsearFecha(dato.getString("created_time")));
+//							temp.put("hora", parsearHora(dato.getString("created_time")));
+							temp.put("description", dato.getDescription());
+							temp.put("user", dato.getUser().getName());
+							
+//						} catch (JSONException e) {
+//						
+//							e.printStackTrace();
+//						}
+						
+
+					list.add(temp);
+				}
+
+
+
+				simpleAdapter = new SimpleAdapter(MyCommentsActivity.this, list, R.layout.my_comments_item, new String[] { "description", "user"},
+			        new int[] { R.id.item_comment_book_title, R.id.item_comment_user_name});
+			        setListAdapter(simpleAdapter);
+			        
+			}else {
+				
+				
+				String msg = "No hay ningun post!";
+				int duracion = Toast.LENGTH_SHORT;
+				
+				Toast toast = Toast.makeText(getApplicationContext(), msg, duracion);
+				int offsetX = 0;
+				int offsetY = 0;
+				
+				toast.setGravity(Gravity.CENTER_HORIZONTAL, offsetX, offsetY);
+				
+				toast.show();
+				
+//				Intent i = new Intent(MisPosteos.this, PostearActivity.class);
+//				startActivity(i);
+					
+				
+			}
+
+
+		}
+			
+		}
+		
+	}
+	
+
--- Library-android/src/com/library/android/mock/BooksMock.java
+++ Library-android/src/com/library/android/mock/BooksMock.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.library.android.domain.Book;
+import com.library.android.domain.Comment;
+import com.library.android.domain.User;
 
 public class BooksMock {
 	
@@ -11,7 +13,9 @@ public class BooksMock {
 		List<Book> list = new ArrayList<Book>();
 		
 		Book book1 = new Book(new Long(12345), "Thinking in Java", null);
+		book1.addComment(new Comment("Feo libroo", UserMock.getUser()));
 		Book book2 = new Book(new Long(6789), "Learning C", null);
+		book2.addComment(new Comment("Bastante bueno", UserMock.getUser()));
 		Book book3 = new Book(new Long(01234), "Visual Basic for Dummies", null);
 		
 		list.add(book1);
